a5e5ba7da07d76aa76ffe6925dc570e17c35c3c4
mary35589 <lanita35589@gmail.com>
2025-03-18 18:04:13 -0500

    Version_02
    Signed-off-by: mary35589 <lanita35589@gmail.com>

--- main.py
+++ main.py
@@ -1,56 +1,62 @@
 from time import sleep
 from machine import Pin
 from machine import mem32
+from machine import ADC
 
-rojopeatonal=Pin(2,Pin.OUT)
-verdepeatonal=Pin(4,Pin.OUT)
-verdevehicular=Pin(13,Pin.OUT)
-amarillovehicular=Pin(12,Pin.OUT)
-rojovehicular=Pin(14,Pin.OUT)
+#Orden de los semaforos
 
+#Semaroro_1: Semaforo especial
+vv_especial= Pin(15, Pin.OUT)
+av_especial= Pin(2, Pin.OUT)
+rv_especial= Pin(0, Pin.OUT)
 
-global variable
-variable=0
+#Semaforo_5: semaforo_vehicular_01 y semaforo_peatonal_01
+vv_01= Pin(4, Pin.OUT)
+av_01= Pin(16, Pin.OUT)
+rv_01= Pin(17, Pin.OUT)
 
-def interrupcion(Pin):
-    global variable
-    print("Entre a la función interrupción")
-    variable=1
+vp_01= Pin(5, Pin.OUT)
+rp_01= Pin(18, Pin.OUT)
 
-pulsador=Pin(23,Pin.IN)
-pulsador.irq(trigger=Pin.IRQ_RISING,handler=interrupcion)
+#Semaforo_2: semaforo_vehicular_02 y semaforo_peatonal_02
+vv_02= Pin(13, Pin.OUT)
+av_02= Pin(12, Pin.OUT)
+rv_02= Pin(14, Pin.OUT)
 
-GPIO_SET=const(0x3FF44004)
+vp_02= Pin(19, Pin.OUT)
+rp_02= Pin(21, Pin.OUT)
+
+#Semaforo_4: semaforo_peatonal_04
+vp_04= Pin(33, Pin.OUT)
+rp_04= Pin(32, Pin.OUT)
+
+#Semaforo_3: semaforo_vehicular_03 y semaforo_peatonal_03
+vv_03= Pin(27, Pin.OUT)
+av_03= Pin(26, Pin.OUT)
+rv_03= Pin(25, Pin.OUT)
+
+vp_03= Pin(22, Pin.OUT)
+rp_03= Pin(23, Pin.OUT)
+
+GPIO_SET=const(0x3FF44004) #posición en memoria
 
 while True:
-    mem32[GPIO_SET]=0b010000000000100 #enciende rojopeatonal y verdevehicular
-    sleep(5)
-    mem32[GPIO_SET]=0B000000000000100 #inicia parpadeo de verdevehicular
-    sleep(0.5)
-    mem32[GPIO_SET]=0B010000000000100
-    sleep(0.5)
-    mem32[GPIO_SET]=0B000000000000100
-    sleep(0.5)
-    mem32[GPIO_SET]=0B010000000000100
-    sleep(0.5)
-    mem32[GPIO_SET]=0B000000000000100
-    sleep(0.5)
-    mem32[GPIO_SET]=0B001000000000100 # amariilo y rojo peatonal
-    sleep(2)
-    mem32[GPIO_SET]=0B000000000000100
-    sleep(0.5)
-    mem32[GPIO_SET]=0B100000000010000 # se enciende rojovehicular y verdepeatonal
-    sleep(5)
-    mem32[GPIO_SET]=0B100000000000000 #parpadeo verdepeatonal
-    sleep(0.5)
-    mem32[GPIO_SET]=0B100000000010000 
-    sleep(0.5)
-    mem32[GPIO_SET]=0B100000000000000 
-    sleep(0.5)
-    mem32[GPIO_SET]=0B010000000010000 # se enciende verde peatonal (opcional)
-    sleep(0.5)
-    if variable==1:
-        mem32[GPIO_SET]=0b10000000000100
-        sleep(10)
-        variable=0
-        #sleep(0.5) #yo lo añadi
+  
+   mem32[GPIO_SET]=0B0100000010011000100010000000100001
+   sleep(5)
+   mem32[GPIO_SET]=0B0100000010011000100000000000100001 #parpadeo semaforo_02 verde
+   sleep(0.5)
+   mem32[GPIO_SET]=0B0100000010011000100010000000100001
+   sleep(0.5)
+   mem32[GPIO_SET]=0B0100000010011000100000000000100001
+   sleep(0.5)
+   mem32[GPIO_SET]=0B0100000010011000100010000000100001
+   sleep(0.5)
+   mem32[GPIO_SET]=0B0100000010011000100000000000100001
+   sleep(0.5) 
+   mem32[GPIO_SET]=0B0100000010011000100001000000100001 #cambio semaforo_02 a amarillo
+   sleep(2)
+###mem32[GPIO_SET]=0b3210987654321098765432109876543210
+   mem32[GPIO_SET]=0B1000001000100010101100000000100000
+   sleep(9)
+
