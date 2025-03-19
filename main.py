4bbc3c9a9a4fca9623de0d5321b7837f99dbe128
mary35589 <lanita35589@gmail.com>
2025-03-19 00:07:06 -0500

    version_04
    Signed-off-by: mary35589 <lanita35589@gmail.com>

--- main.py
+++ main.py
@@ -38,10 +38,27 @@ rv_03= Pin(25, Pin.OUT)
 vp_03= Pin(22, Pin.OUT)
 rp_03= Pin(23, Pin.OUT)
 
+s_temperatura=ADC(Pin(36))
+s_temperatura.atten(ADC.ATTN_11DB)
+
+conversion= 3.3/4095*100
+
 GPIO_SET=const(0x3FF44004) #posición en memoria
 
+global variable, muestra_temp
+variable=0
+muestra_temp= False 
+
+def interrupcion(Pin):
+    global variable
+    print("Entre a la funcion interrupcion")
+    variable=1  
+
+pul_interrupcion=Pin(35,Pin.IN,)
+pul_interrupcion.irq(trigger=Pin.IRQ_FALLING, handler=interrupcion)
+
+
 while True:
-  
    mem32[GPIO_SET]=0B0100000010011000100010000000100001 #semaforo_02=verde semaforo_03=rojo semaforo_05=rojo semaforo_especial=rojo
    sleep(9)
    mem32[GPIO_SET]=0B0100000010011000100000000000100001 #parpadeo semaforo_02=verde
@@ -82,6 +99,13 @@ while True:
    sleep(0.5)
    mem32[GPIO_SET]=0B0100000010010011000100000000000000
    sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010010011010100000000000100 #cambio semaforo_especial a amarillo semaforo_05=amarillo
+   mem32[GPIO_SET]=0B0100000010010011010100000000000100#cambio semaforo_especial a amarillo semaforo_05=amarillo
+   sleep(2)
+
+   if variable==1:
+      mem32[GPIO_SET]=0B1000000010010010100100000000100001 
+      sleep(10)
+      variable=0
 ###mem32[GPIO_SET]=0b3210987654321098765432109876543210
+
 
