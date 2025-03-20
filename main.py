b86c90b5d56817114e676e9f9c3a3b1c424ddab9
mary35589 <lanita35589@gmail.com>
2025-03-20 07:25:26 -0500

    version_terminada_interrupcion
    Signed-off-by: mary35589 <lanita35589@gmail.com>

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ rv_03= Pin(25, Pin.OUT)
 vp_03= Pin(22, Pin.OUT)
 rp_03= Pin(23, Pin.OUT)
 
-s_temperatura=ADC(Pin(36))
-s_temperatura.atten(ADC.ATTN_11DB)
+sensor_temperatura=ADC(Pin(35))
+sensor_temperatura.atten(ADC.ATTN_11DB)
 
 conversion= 3.3/4095*100
 
@@ -52,60 +52,80 @@ muestra_temp= False
 def interrupcion(Pin):
     global variable
     print("Entre a la funcion interrupcion")
-    variable=1  
+    variable=1 
 
-pul_interrupcion=Pin(35,Pin.IN,)
-pul_interrupcion.irq(trigger=Pin.IRQ_FALLING, handler=interrupcion)
+def temperatura(Pin): 
+   global muestra_temp
+   if muestra_temp:
+      muestra_temp=False
+      print("No se va a tomar temperatura")
 
+   else:
+      muestra_temp=True
+      print("Se va a tomar temperatura")
+
+pul_interrupcion=Pin(34,Pin.IN,Pin.PULL_DOWN)
+pul_interrupcion.irq(trigger=Pin.IRQ_RISING, handler=interrupcion)
+
+pul_sensor=Pin(32,Pin.IN, Pin.PULL_DOWN)
+pul_sensor.irq(trigger=Pin.IRQ_RISING, handler=temperatura)
 
 while True:
-   mem32[GPIO_SET]=0B0100000010011000100010000000100001 #semaforo_02=verde semaforo_03=rojo semaforo_05=rojo semaforo_especial=rojo
-   sleep(9)
-   mem32[GPIO_SET]=0B0100000010011000100000000000100001 #parpadeo semaforo_02=verde
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010011000100010000000100001
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010011000100000000000100001
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010011000100010000000100001
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010011000100000000000100001
-   sleep(0.5) 
-   mem32[GPIO_SET]=0B0100000010011000100001000000100001 #cambio semaforo_02 a amarillo
-   sleep(2)
-   mem32[GPIO_SET]=0B1000001000100011101100000000000000 #semaforo_03=verde semaforo_02=rojo semaforo_05=rojo semaforo_especial=verde 
-   sleep(7)
-   mem32[GPIO_SET]=0B1000000000100011101100000000000000 #parpadeo semaforo_03=verde
-   sleep(0.5)
-   mem32[GPIO_SET]=0B1000001000100011101100000000000000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B1000000000100011101100000000000000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B1000001000100011101100000000000000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B1000000000100011101100000000000000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B1000000100100011101100000000000000 #cambio semaforo_03 a amarillo
-   sleep(2)
-   mem32[GPIO_SET]=0B0100000010010011001100000000010000 #semaforo_especial=verde semaforo_02=rojo semaforo_03=rojo semaforo_05=verde
-   sleep(9) 
-   mem32[GPIO_SET]=0B0100000010010011000100000000000000 #parpadeo semaforo_especial=verde y semaforo_05=verde
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010010011001100000000010000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010010011000100000000000000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010010011001100000000010000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010010011000100000000000000
-   sleep(0.5)
-   mem32[GPIO_SET]=0B0100000010010011010100000000000100#cambio semaforo_especial a amarillo semaforo_05=amarillo
-   sleep(2)
+   if muestra_temp:
+      toma= sensor_temperatura.read()
+      temperatura = toma*conversion #para convertir la muestra a grados celsius
+      print("La temperatura es: ", temperatura, "°C")
+   else: 
+      mem32[GPIO_SET]=0B0100000010011000100010000000100001 #semaforo_02=verde semaforo_03=rojo semaforo_05=rojo semaforo_especial=rojo
+      sleep(9)
+      mem32[GPIO_SET]=0B0100000010011000100000000000100001 #parpadeo semaforo_02=verde
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010011000100010000000100001
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010011000100000000000100001
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010011000100010000000100001
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010011000100000000000100001
+      sleep(0.5) 
+      mem32[GPIO_SET]=0B0100000010011000100001000000100001 #cambio semaforo_02 a amarillo
+      sleep(2)
+      mem32[GPIO_SET]=0B1000001000100011101100000000000000 #semaforo_03=verde semaforo_02=rojo semaforo_05=rojo semaforo_especial=verde 
+      sleep(7)
+      mem32[GPIO_SET]=0B1000000000100011101100000000000000 #parpadeo semaforo_03=verde
+      sleep(0.5)
+      mem32[GPIO_SET]=0B1000001000100011101100000000000000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B1000000000100011101100000000000000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B1000001000100011101100000000000000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B1000000000100011101100000000000000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B1000000100100011101100000000000000 #cambio semaforo_03 a amarillo
+      sleep(2)
+      mem32[GPIO_SET]=0B0100000010010011001100000000010000 #semaforo_especial=verde semaforo_02=rojo semaforo_03=rojo semaforo_05=verde
+      sleep(9) 
+      mem32[GPIO_SET]=0B0100000010010011000100000000000000 #parpadeo semaforo_especial=verde y semaforo_05=verde
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010010011001100000000010000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010010011000100000000000000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010010011001100000000010000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010010011000100000000000000
+      sleep(0.5)
+      mem32[GPIO_SET]=0B0100000010010011010100000000000100#cambio semaforo_especial a amarillo semaforo_05=amarillo
+      sleep(2)
 
-   if variable==1:
-      mem32[GPIO_SET]=0B1000000010010010100100000000100001 
-      sleep(10)
-      variable=0
-###mem32[GPIO_SET]=0b3210987654321098765432109876543210
+      if variable==1:
+         mem32[GPIO_SET]=0B1000000010010010100100000000100001
+         print("Esta funcionando")
+         sleep(15)
+         variable=0
+   ###mem32[GPIO_SET]=0b3210987654321098765432109876543210
+
+ 
 
 
